18cee4990a59d7735eada9915ab13f59c66e4b0b
John Con <john dow@gmail.com>
2023-06-21 23:10:06 +0300

    Switch recon loss BCE→MSE

--- distributed_vae/src/train_vae.py
+++ distributed_vae/src/train_vae.py
@@ -73,3 +73,4 @@ if task.get("index",0)==0:
 # Improve launch script GPU mask (2024-10-08T02:58:32)
 # Track recon & KL separately (2024-08-25T11:46:30)
 # CLI flag for batch size (2025-01-29T18:59:09)
+# Switch recon loss BCE→MSE (2023-06-21T23:10:06)

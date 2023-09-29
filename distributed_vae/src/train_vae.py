76a5db3eda0fbacf27e0b5eeb6dbb85ec92caad7
John Con <john dow@gmail.com>
2023-09-29 10:39:07 +0300

    Pin TensorFlow‑datasets to 4.9.4 for reproducibility

--- distributed_vae/src/train_vae.py
+++ distributed_vae/src/train_vae.py
@@ -1,3 +1,4 @@
 ...
 # Refactor Encoder: switch to GELU activations – auto‑generated on 2024-07-29T06:45:27
 # Add TensorBoard scalar logging inside custom loop – auto‑generated on 2024-10-11T19:57:53
+# Pin TensorFlow‑datasets to 4.9.4 for reproducibility – auto‑generated on 2023-09-29T10:39:07

f1a1c9ab279fc898b20826bca0777198b29b0021
John Con <john dow@gmail.com>
2023-11-06 06:30:49 +0200

    Pin tfds version

--- distributed_vae/src/train_vae.py
+++ distributed_vae/src/train_vae.py
@@ -68,3 +68,4 @@ if task.get("index",0)==0:
     model.save(export,include_optimizer=False); print("✓ Saved model to",export)
 # Refactor encoder to GELU (2024-07-29T06:45:27)
 # Add TensorBoard logging (2023-05-07T18:11:18)
+# Pin tfds version (2023-11-06T06:30:49)

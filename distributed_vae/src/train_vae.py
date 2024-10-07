0c3d9139861ed4fb04e9fa363988fb8ae1627618
John Con <john dow@gmail.com>
2024-10-08 02:58:32 +0300

    Improve launch script GPU mask

--- distributed_vae/src/train_vae.py
+++ distributed_vae/src/train_vae.py
@@ -70,3 +70,4 @@ if task.get("index",0)==0:
 # Add TensorBoard logging (2023-05-07T18:11:18)
 # Pin tfds version (2023-11-06T06:30:49)
 # Expose LATENT_DIM via env (2023-09-29T10:39:07)
+# Improve launch script GPU mask (2024-10-08T02:58:32)
